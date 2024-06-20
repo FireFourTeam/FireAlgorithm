134c3be0f01b106b36fa64e3f51fa42a3f7833dc
judy-kimsohui <sohuikim2020@naver.com>
2024-06-20 22:41:27 +0900

    add judy thu

--- Week2 Greedy (그리디)/4목 G 2285 우체국/judy.py
+++ Week2 Greedy (그리디)/4목 G 2285 우체국/judy.py
@@ -4,20 +4,24 @@ input = sys.stdin.readline
 N = int(input())
 A_sum = 0
 
+# 일직선 마을 = line
 line = []
-for i in range(N) :
+for i in range(N) : # 입력과 전체 사람 수 구하기
     X, A = map(int, input().split())
     line.append([X,A])
     A_sum += A
 
-A_check_halfsum = 0
-half_X = 0
-line.sort(key= lambda x: x[0])
+A_check_halfsum = 0 # 사람들 절반넘어가는 것 측정 변수
+half_X = 0 # 결과 위치
+
+line.sort(key= lambda x: x[0]) #이런 문제 특 : 순서대로 안줌
 for X, A in line :
     A_check_halfsum += A
-    if A_check_halfsum >= A_sum/2 :
+    if A_check_halfsum >= A_sum/2 : #처음으로 절반 넘어가는 것
         half_X = X 
         break
     
 # 중앙값
 print(int(half_X))
+
+# 질량 중심이 아닌 이유.. ㅠ
