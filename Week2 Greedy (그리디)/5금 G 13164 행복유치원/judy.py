e3139cfdb92dc360f7145a97dd0e65d00353e8a9
judy-kimsohui <sohuikim2020@naver.com>
2024-06-21 22:37:45 +0900

    add judy

---
+++ Week2 Greedy (그리디)/5금 G 13164 행복유치원/judy.py
@@ -0,0 +1,38 @@
+# 차례대로 학생을 포함/비포함 시키면서 최소 비용을 찾는다. O(n)
+
+N, K = map(int, input().split())
+babies_tall = list(map(int, input().split())) #tall
+
+# [LBaby, idx, RBaby, idx, gap]
+chart = []
+for idx, baby in enumerate(babies_tall) :
+    if idx == (len(babies_tall)-1) :
+        break
+    chart.append([babies_tall[idx], idx, babies_tall[idx+1], idx+1, babies_tall[idx+1]-babies_tall[idx]])
+
+chart.sort(key= lambda x: (-x[4], -x[2]))
+
+cost = 0
+cost_chart = []
+for i in range(K-1) :
+    cost_chart.append(chart[i])
+    cost += chart[i][0] #left
+    cost -= chart[i][2] #right
+
+cost_chart.sort(key= lambda x: x[0])
+
+# 첫번째 구분선 왼쪽이 첫 아이가 아니라면, 첫번째 아이를 빼줘야함
+if cost_chart[0][1] != 0 :
+    cost -= babies_tall[0]
+# 첫번째 구분선 왼쪽이 첫 아이라면, 더해야 하는데, 이미 더했으므로 놔둠.
+else :
+    pass
+# 마지막 구분선 오른쪽이 마지막 아이가 아니라면, 마지막 아이를 더해줘야함
+
+if cost_chart[K-2][3] != (len(babies_tall)-1) :
+    cost += babies_tall[len(babies_tall)-1]
+# 마지막 구분선 오른쪽이 마지막 아이라면, 빼지 않고 더해야함
+else :
+    cost += babies_tall[len(babies_tall)-1] * 2
+    
+print(cost)
