89d31808f255d5fd4dc1199e23f3d56ba37a2ba4
KakaoCloud <user@KakaoCloudui-MacBookPro.local>
2024-06-27 22:29:43 +0900

    2024-06-27

--- Week3 Backtracking (백트리킹)/4목 G 10597 순열장난/andy.py
+++ Week3 Backtracking (백트리킹)/4목 G 10597 순열장난/andy.py
@@ -1,47 +1,34 @@
-import sys,heapq
+import sys
 
 input = sys.stdin.readline
 
-def dfs(idx,prev,dct,cnt):
-    global ans
-    if ans != None:
+lst = input().strip()
+
+n = len(lst)
+
+max_num = n if n<=9 else 9 + (n-9)//2
+
+arr = []
+
+done = False
+
+def dfs(idx):
+    global arr,done
+    if done:
         return
-    
     if idx == len(lst):
-        num = 1
-        while(1):
-            if num in dct.keys():
-                num+=1
-            else:
-                break
-        if num == cnt:
-            ans = 1
-            for ki in dct.keys():
-                print(ki)
+        done = True
+        print(*arr)
         return
     
-    if prev != None:
-        new_num = int(str(prev)+str(lst[idx]))
-        if new_num not in dct.keys():
-            dct[new_num] = 1
-        else:
-            return
-        dfs(idx+1,None,dct,cnt+1)
-        del dct[new_num]
-    elif int(lst[idx]) == 0:
-        return
-    elif int(lst[idx]) not in dct.keys():
-        dct[int(lst[idx])] = 1
-        dfs(idx+1,None,dct,cnt+1)
-        del dct[int(lst[idx])]
+    if lst[idx] != '0' and lst[idx] not in arr and int(lst[idx])<=max_num:
+        arr.append(lst[idx])
+        dfs(idx+1)
+        arr.pop()
+    
+    if lst[idx] != '0' and idx+1 < len(lst) and lst[idx:idx+2] not in arr and int(lst[idx:idx+2])<=max_num:
+        arr.append(lst[idx:idx+2])
+        dfs(idx+2)
+        arr.pop()
 
-        if idx<len(lst)-1:
-            dfs(idx+1,lst[idx],dct,cnt)
-
-lst = list(input().strip())
-
-dct = {}
-
-ans = None
-
-dfs(0,None,dct,0)
+dfs(0)
