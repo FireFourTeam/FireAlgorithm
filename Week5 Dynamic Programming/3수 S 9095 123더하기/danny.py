bfd093dd01a1dced2e8c34238ab8a19d948b7a6d
dannycho1530 <46494084+dannycho1530@users.noreply.github.com>
2024-07-10 22:12:19 +0900

    Create danny.py

---
+++ Week5 Dynamic Programming/3수 S 9095 123더하기/danny.py
@@ -0,0 +1,18 @@
+import sys
+
+def dp(n):
+	if n == 1:
+		return 1
+	elif n == 2:
+		return 2
+	elif n == 3:
+		return 4
+	else:
+		return dp(n-1) + dp(n-2) + dp(n-3)
+
+
+T = int(sys.stdin.readline().strip())
+
+for _ in range(T):
+	n = int(sys.stdin.readline().strip())
+	print(dp(n))
